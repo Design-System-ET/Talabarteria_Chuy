31e69ba5ccdefc86edfceecd26367d1ed87c5317
Design-System-ET <darkman_anubis_iara@hotmail.com>
2026-06-04 19:08:31 -0300

    Iniciamos el proyecto por index.html

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/Talabarteria_Chuy/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        contacto: resolve(__dirname, "pages/contacto.html")
+        contacto: resolve(__dirname, "pages/contacto.html"),
+        store: resolve(__dirname, "pages/store.html")
       }
     }
   }
