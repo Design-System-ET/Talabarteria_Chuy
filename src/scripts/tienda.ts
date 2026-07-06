a0ae7a073e9bf7c9fbaf5864072a1449c9d4b258
Design <darkman_anubis_iara@hotmail.com>
2026-07-06 15:06:04 -0300

    Agregamos productos, y efecto de carga

--- src/scripts/tienda.ts
+++ src/scripts/tienda.ts
@@ -1,3 +1,9 @@
+//efecto al cargar la tienda
+import {gsap} from "gsap";
+import { ScrollTrigger} from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
 import Swal from "sweetalert2";
 import { Producto } from '../interfaces/Producto';
 
@@ -22,22 +28,39 @@ fetch('/Talabarteria_Chuy/productos.json')
                     <div class="card" style="width: 18rem;">
                     <img src="${p.imagen}" class="card-img-top img-producto img_tienda" alt="${p.titulo}" data-img="${p.imagen}">
                     <div class="card-body">
-                        <h5 class="card-title">${p.titulo}</h5>
-                        <p class="card-text">${p.descripcion}</p>
+                        <h5 class="card-title" style="font-weight: 900;">${p.titulo}</h5>
+                        <p class="card-text" style="text-align: justify; height: auto;">${p.descripcion}</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">${p.item1}</li>
-                        <li class="list-group-item">${p.item2}</li>
-                        <li class="list-group-item">${p.item3}</li>
+                        
+                        <li class="list-group-item" style="font-weight: 900; color: green; text-align: right;">${p.item3}</li>
                     </ul>
                     <div class="card-body">
                         <a href="${cel}" target="_blank" class="btn btn-primary">Comprar</a>
-                        <a href="${p.texto2}" class="card-link text-center">${p.texto2}</a>
+                        <!-- <a href="${p.texto2}" class="card-link text-center">${p.texto2}</a> -->
                     </div>
                 </div>
                 </div>
             `;
         });
+
+        // 👇 efecto al cargar la tienda
+        const cards = contenedor.querySelectorAll('.card');
+        gsap.utils.toArray('.card').forEach((card: any) => {
+            gsap.from(card, {
+                y: -100,
+                opacity: 0,
+                scale: 0.1,
+                scrollTrigger: {
+                    trigger: card,
+                    start: "top 90%",
+                    end: "top 60%",
+                    scrub: 5,
+                },
+            });
+
+
+    });
 
         // 👇 evento después de renderizar
         contenedor.addEventListener('click', (e) => {
@@ -55,6 +78,3 @@ fetch('/Talabarteria_Chuy/productos.json')
 
     })
     .catch(err => console.error('Error cargando productos:', err));
-
-
-
